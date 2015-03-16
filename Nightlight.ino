3974e0e313f6e0abf6aff3f0b921ae50b2621e25
Bryan Smith <zarboz@gmail.com>
2015-03-16 15:36:11 -0500

    In my editing haste I forgot to declare what colorWipe does
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

--- Nightlight.ino
+++ Nightlight.ino
@@ -34,3 +34,11 @@ void loop() {
   lastswitch1state = currswitch1state;
 }
 
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
