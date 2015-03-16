cd38422498d4869c7d542e07ce4ffc84a5b07149
Bryan Smith <zarboz@gmail.com>
2015-03-16 15:32:53 -0500

    drop the secondary switch for off and make it all software based off 1 single momentary switch
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

--- Nightlight.ino
+++ Nightlight.ino
@@ -6,11 +6,7 @@ const int onbuttonPin = 3;
 //int buttonState1 = 0;
 boolean currswitch1state = LOW;
 boolean lastswitch1state = LOW;
-
-const int offbuttonPin = 2;
-//int buttonState2 = 0;
-boolean currswitch2state = LOW;
-boolean lastswitch2state = LOW;
+boolean lampState = FALSE;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -25,24 +21,19 @@ void setup() {
 void loop() {
   currswitch1state = digitalRead(onbuttonPin);
   currswitch2state = digitalRead(offbuttonPin);
-  if (currswitch1state == HIGH && lastswitch1state == LOW) {
-  Serial.println("Lights on");
-  Serial.println(currswitch1state);
-  colorWipe(strip.Color(255, 255, 255), 25);
+  if (currswitch1state == HIGH && lastswitch1state == LOW) 
+  {
+   lampState = !lampState;  // toggle the lamp state
   }
-   if (currswitch2state == HIGH && lastswitch2state == LOW) {
-     Serial.println("Switch 2 on");
-  Serial.println(currswitch2state);
-  colorWipe(strip.Color(0, 0, 0), 5); 
-   }
-}
-
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-      strip.show();
-      delay(wait);
+  if (lampState == true)
+  {
+     colorWipe(strip.Color(255, 255, 255), 25);  // lamp on
+  }
+  else
+  {
+     colorWipe(strip.Color(0, 0, 0), 25);  // lamp off
   }
   lastswitch1state = currswitch1state;
   lastswitch2state = currswitch2state;
 }
+
