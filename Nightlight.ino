f8f289ecfc1c4657a9a01f219a31b85f134369dc
Bryan Smith <mugenman1111@gmail.com>
2015-03-27 01:12:44 -0500

    Finish up some commenting

--- Nightlight.ino
+++ Nightlight.ino
@@ -1,12 +1,19 @@
+//TODO: setup "modes" with the single switch 
+//To have 3 seperate dim states or colors
+
 #include <Adafruit_NeoPixel.h>
 #define PIN 4
 #define NUMPIXELS 16
 
 const int onbuttonPin = 3;
-//int buttonState1 = 0;
 boolean currswitch1state = LOW;
 boolean lastswitch1state = LOW;
 boolean lampState = false;
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -25,11 +32,11 @@ void loop() {
   }
   if (lampState == true)
   {
-     colorWipe(strip.Color(255, 255, 255), 25);  // lamp on
+     colorWipe(strip.Color(255, 255, 255), 25);  // lamp on define color in the 3 rows R,G,B value or use another neopixel action listed below
   }
   else
   {
-     colorWipe(strip.Color(0, 0, 0), 25);  // lamp off
+     colorWipe(strip.Color(0, 0, 0), 25);  // lamp off turn all lights off from wiping them on
   }
   lastswitch1state = currswitch1state;
 }
@@ -41,4 +48,82 @@ void colorWipe(uint32_t c, uint8_t wait) {
       delay(wait);
   }
 }
+/*
+*more neopixel functions from Adafruits strandtest library
+*these will let you have more freedom for your color and playing.
 
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+     
+      delay(wait);
+     
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+        }
+        strip.show();
+       
+        delay(wait);
+       
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, 0);        //turn every third pixel off
+        }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else if(WheelPos < 170) {
+    WheelPos -= 85;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+}*/
